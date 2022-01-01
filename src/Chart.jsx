d6e4ecb2ad752821939b20cedec21788b10021dc
Kosteyyy <averkiev@yandex.ru>
2022-01-01 17:54:26 +0300

    heroku try canvas any color from array new other quotes

--- src/Chart.jsx
+++ src/Chart.jsx
@@ -3,36 +3,36 @@ import React, { useEffect } from "react";
 export default function Chart({ results, fillColors, borderColors }) {
   console.log(fillColors);
   let colors = [...fillColors];
-    useEffect(() => {
-      let cx = document.querySelector("canvas").getContext("2d");
-      let total = results.reduce((sum, { count }) => sum + count, 0);
-      console.log("total = ", total);
-      let currentAngle = -0.5 * Math.PI;
-      let sliceAngle = (results[0].count / total) * 2 * Math.PI;
-      cx.beginPath();
-      cx.arc(101, 101, 100, currentAngle, currentAngle + sliceAngle);
-      currentAngle += sliceAngle;
-      cx.lineTo(101, 101);
-      // cx.moveTo(50, 10);
-      // cx.lineTo(10, 70);
-      // cx.lineTo(90, 70);
-      cx.fill();
-      cx.stroke();
-      sliceAngle = (results[1].count / total) * 2 * Math.PI;
-      cx.beginPath();
-      cx.arc(101, 101, 100, currentAngle, currentAngle + sliceAngle);
-      currentAngle += sliceAngle;
-      cx.lineTo(101, 101);
-      // cx.moveTo(50, 10);
-      // cx.lineTo(10, 70);
-      // cx.lineTo(90, 70);
-      // console.log(fillColors[1], borderColors[1]);
-        cx.fillStyle = colors[0];
-        console.log(colors[0], typeof colors[0]);
-        cx.strokeStyle = "#2176ae";
-      cx.fill();
-      cx.stroke();
-    }, [])
+    // useEffect(() => {
+    //   let cx = document.querySelector("canvas").getContext("2d");
+    //   let total = results.reduce((sum, { count }) => sum + count, 0);
+    //   console.log("total = ", total);
+    //   let currentAngle = -0.5 * Math.PI;
+    //   let sliceAngle = (results[0].count / total) * 2 * Math.PI;
+    //   cx.beginPath();
+    //   cx.arc(101, 101, 100, currentAngle, currentAngle + sliceAngle);
+    //   currentAngle += sliceAngle;
+    //   cx.lineTo(101, 101);
+    //   // cx.moveTo(50, 10);
+    //   // cx.lineTo(10, 70);
+    //   // cx.lineTo(90, 70);
+    //   cx.fill();
+    //   cx.stroke();
+    //   sliceAngle = (results[1].count / total) * 2 * Math.PI;
+    //   cx.beginPath();
+    //   cx.arc(101, 101, 100, currentAngle, currentAngle + sliceAngle);
+    //   currentAngle += sliceAngle;
+    //   cx.lineTo(101, 101);
+    //   // cx.moveTo(50, 10);
+    //   // cx.lineTo(10, 70);
+    //   // cx.lineTo(90, 70);
+    //   // console.log(fillColors[1], borderColors[1]);
+    //     cx.fillStyle = colors[0];
+    //     console.log(colors[0], typeof colors[0]);
+    //     cx.strokeStyle = "#2176ae";
+    //   cx.fill();
+    //   cx.stroke();
+    // }, [])
 
   // useEffect(() => {
   //   let cx = document.querySelector("canvas").getContext("2d");
@@ -55,7 +55,13 @@ export default function Chart({ results, fillColors, borderColors }) {
   //   }
   // }, [results, fillColors, borderColors]);
 
-  return <canvas width="202" height="202"></canvas>;
+  return (
+    <div>
+      <div>{fillColors[0]}</div>
+      <canvas width="202" height="202"></canvas>;
+    </div>
+  )
+
 }
 
 // import { Chart } from "react-google-charts";
