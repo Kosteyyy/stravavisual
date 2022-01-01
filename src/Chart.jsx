e5385baf03c84f9ba8f2cd287a7644e366e88845
Kosteyyy <averkiev@yandex.ru>
2022-01-01 17:15:13 +0300

    heroku try canvas for mobile chrome setFillColor

--- src/Chart.jsx
+++ src/Chart.jsx
@@ -25,7 +25,8 @@ export default function Chart({ results, fillColors, borderColors }) {
       // cx.lineTo(10, 70);
       // cx.lineTo(90, 70);
       console.log(fillColors[1], borderColors[1]);
-        cx.fillStyle = "red";//fillColors[1];
+        cx.fillStyle = "red";
+        setFillColor("red");//fillColors[1];
         // cx.strokeStyle = borderColors[1];
       cx.fill();
       cx.stroke();
