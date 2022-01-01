ddb30116d3528e400d386145dafdf8b43a16649c
Kosteyyy <averkiev@yandex.ru>
2022-01-01 17:27:36 +0300

    heroku try canvas for mobile chrome remove colors

--- src/Chart.jsx
+++ src/Chart.jsx
@@ -24,7 +24,7 @@ export default function Chart({ results, fillColors, borderColors }) {
       // cx.moveTo(50, 10);
       // cx.lineTo(10, 70);
       // cx.lineTo(90, 70);
-      console.log(fillColors[1], borderColors[1]);
+      // console.log(fillColors[1], borderColors[1]);
         // cx.fillStyle = 'red';
         // cx.strokeStyle = borderColors[1];
       cx.fill();
