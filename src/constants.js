eba7ad7e857bd7f0ff96d16a1b9bbf2568a272ba
Kosteyyy <averkiev@yandex.ru>
2022-01-03 11:12:31 +0300

    colors set diagram

--- src/constants.js
+++ src/constants.js
@@ -20,7 +20,7 @@ const CHART_COLORS = [
     { colors: ['#2176ae',"#fbb13c","#57b8ff","#eee5e9","#493b2a","#d64933","#593f62","#684e32","#fe6847","#f75590","#fce4d8","#fbd87f"], borders: ["#2176ae","#a06403","#57b8ff","#b690a1","#493b2a","#d64933","#593f62","#684e32","#fe6847", "#f75590","#fce4d8","#fbd87f"] },
     { colors: ["#2f3061","#ffe66d","#6ca6c1","#f7fff7","#efa00b","#d65108","#a18276","#fbacbe","#fa9500"], borders: ["#2f3061","#ccaa00","#6ca6c1","#6ca6c1","#efa00b","#d65108","#a18276","#fbacbe","#fa9500"] },
     { colors: ["#f75590","#fce4d8","#fbd87f","#b5f8fe","#10ffcb","#074f57","#077187","#74a57f","#7cc6fe"], borders: ["#f75590","#fce4d8","#fbd87f","#b5f8fe","#10ffcb","#074f57","#077187","#74a57f","#7cc6fe"] },
-    { colors: ['#fff7ec','#fdd49e','#fdbb84','#fc8d59','#ef6548','#fee8c8','#d7301f','#b30000','#7f0000'], borders: ["#fdbb84","#fdbb84","#fdbb84",'#fdbb84','#fc8d59','#ef6548','#d7301f','#b30000','#7f0000'] },
+    { colors: ['#6db8bb','#3c5b5c','#cae2e3','#c4cecf','#ba9607','#f4d75e','#91a0a1','#c1f6f7','#039ba1', '#f7dc9c','#e69f12','#8c784a'], borders: ['#6db8bb','#3c5b5c','#cae2e3','#c4cecf','#ba9607','#f4d75e','#91a0a1','#c1f6f7','#039ba1', '#f7dc9c','#e69f12','#8c784a'] },
     { colors: ['#d73027','#f46d43','#fdae61','#fee090','#ffffbf','#e0f3f8','#abd9e9','#74add1','#4575b4'], borders: ['#d73027','#f46d43','#f46d43','#fdae61','#fdae61','#74add1','#74add1','#74add1','#4575b4'] },
     { colors: ['#8c510a','#bf812d','#dfc27d','#f6e8c3','#f5f5f5','#c7eae5','#80cdc1','#35978f','#01665e'], borders: ['#8c510a','#bf812d','#bf812d','#dfc27d','#dfc27d','#35978f','#35978f','#35978f','#01665e'] },
 ];
